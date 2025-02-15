e351afa3251c9c7dc5de425468d606fa4ce19cae
Raja V <rajamails19@gmail.com>
2025-02-15 12:02:01 -0500

    Create NumberCounter.py

---
+++ NumberCounter.py
@@ -0,0 +1,8 @@
+def count_numbers():
+    start = int(input("Start number: "))
+    end = int(input("End number: "))
+    
+    print("Here we go! 🚀")
+    for num in range(start, end + 1):
+        input(f"Press Enter to see next number")
+        print(f"Number: {num} ✨")
