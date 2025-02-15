2e6d44c03dcc70522bce1f0bf1e2c70ca499dec0
Raja V <rajamails19@gmail.com>
2025-02-15 12:00:37 -0500

    Create RainbowColors.py

---
+++ RainbowColors.py
@@ -0,0 +1,10 @@
+def rainbow():
+    colors = ["Red 🔴", "Orange 🟠", "Yellow 🟡", 
+              "Green 🟢", "Blue 🔵", "Purple 🟣"]
+    print("Let's make a rainbow!")
+    
+    for color in colors:
+        input(f"Press Enter to add {color}")
+        print(f"Adding {color} to our rainbow!")
+    
+    print("Look at our beautiful rainbow! 🌈")
